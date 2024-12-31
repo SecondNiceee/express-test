78ac564a30088670b3aa479936015f9f354fdd64
SecondNiceee <col1596321@gmail.com>
2024-12-31 13:31:40 +0300

    dsa

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", router)
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 
 async function startApp() {
