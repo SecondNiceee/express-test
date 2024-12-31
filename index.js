a78a0eae65d897c3196bdd5a75a806b3fe94d4b5
SecondNiceee <col1596321@gmail.com>
2024-12-31 20:56:12 +0300

    dsad

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express"
 import mongoose from "mongoose"
 import router from "./router.js"
 import fileUpload from "express-fileupload"
-
+import path from "path"
+import { fileURLToPath } from 'url';
 
 const PORT = 5000 
 
@@ -10,13 +11,17 @@ const DB_URL = 'mongodb+srv://col1596321:wNX8dJcmrgCW1faq@cluster0.t3b11.mongodb
 
 const app = express()
 
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 // app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router)
 
-// app.use(express.static(p ath.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 
 async function startApp() {
